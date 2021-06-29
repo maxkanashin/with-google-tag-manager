ca3f2ea0468ae3d0b9c67a8ca4855cffd8221d05
maxim_kanashin <maxim.kanashin@omega-r.com>
2021-06-29 22:34:06 +0300

    Revert "Update GoogleTagManager.js"
    This reverts commit 91d99842e31a5829eef0710b8fe70c54af82e546.

--- components/GoogleTagManager.js
+++ components/GoogleTagManager.js
@@ -5,12 +5,12 @@ import * as gtm from '../lib/gtm'
 const GoogleTagManager = ({ children }) => {
   const router = useRouter()
 
-  // useEffect(() => {
-  //   router.events.on('routeChangeComplete', gtm.pageview)
-  //   return () => {
-  //     router.events.off('routeChangeComplete', gtm.pageview)
-  //   }
-  // }, [router.events])
+  useEffect(() => {
+    router.events.on('routeChangeComplete', gtm.pageview)
+    return () => {
+      router.events.off('routeChangeComplete', gtm.pageview)
+    }
+  }, [router.events])
 
   return children
 }
